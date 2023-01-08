50ef256910d44742cd805fabc7d60f46c78e4541
Hamid Adesokan <aadehamid@gmail.com>
2023-01-08 08:46:45 -0600

    Cleaned up bash scripts. Removed commented sections.

--- install-packages.sh
+++ install-packages.sh
@@ -17,29 +17,11 @@ apt-get -y upgrade
 # Install a new package, without unnecessary recommended packages:
 # apt-get -y install --no-install-recommends syslog-ng
 
-# # Install Docker CE CLI
-# apt-get update \
-#     && apt-get install -y apt-transport-https ca-certificates curl gnupg2 lsb-release \
-#     && curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/gpg | apt-key add - 2>/dev/null \
-#     && echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list \
-#     && apt-get update \
-#     && apt-get install -y docker-ce-cli
-
-# # Install Docker Compose
-# export LATEST_COMPOSE_VERSION=$(curl -sSL "https://api.github.com/repos/docker/compose/releases/latest" | grep -o -P '(?<="tag_name": ").+(?=")') \
-#     && curl -sSL "https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
-#     && chmod +x /usr/local/bin/docker-compose
-
-
 export DOCKER_BUILDKIT=1
 
 # Install Git
 apt-get update \
     && apt-get install -y git
-
-
-
-
 
 # Delete cached files we don't need anymore (note that if you're
 # using official Docker images for Debian or Ubuntu, this happens
